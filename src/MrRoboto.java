7b24fd5a093ecfcee21cef2c5576916ac8c2cae4
Atticus Blanco <atticusbrujx@gmail.com>
2023-09-05 11:40:24 -0700

    Code instructions commented out into code
    Warning:(11, 17) Variable 'scanner' is never used
    
    Warning:(14, 16) Variable 'domo' is never used

--- src/MrRoboto.java
+++ src/MrRoboto.java
@@ -17,3 +17,20 @@ public class MrRoboto {
         // Write your program here
     }
 }
+
+/*
+Modify MrRoboto.java to do the following:
+
+Where the comment // Write your program here exists, use the scanner instance to read standard in and wait for the user to press Enter.
+
+After the enter key is pressed, your program should print the string typed by the user, initially you, concatenated to the String variable domo.
+
+See Oracle's Scanner Javadoc to read all the data types Scanner can read (You want to read a String)
+
+Hint: you're looking for a method -> nextLine
+
+Output should look something like this:
+
+My name is Mr. Roboto, are you Kilroy? no, I'm stan
+Domo arigato no, I'm stan
+ */
