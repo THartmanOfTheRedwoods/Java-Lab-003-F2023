e5f9598957febb822b8a9f4c5842c4e247ba5ad3
Atticus Blanco <atticusbrujx@gmail.com>
2023-09-05 12:19:13 -0700

    Instructor's comments deleted Author tags added

--- src/MrRoboto.java
+++ src/MrRoboto.java
@@ -5,32 +5,38 @@ public class MrRoboto {
     /**
      * Teaching Mr. Roboto how to listen to us.
      * @param args Command line arguments [The source file path, The target file path, ...]
+     *      * @author Trevor Hartman
+     *      * @author Atticus Blanco
+     *      *
+     *      * @since Version 1.0
      **/
     public static void main(String[] args) {
-        // The instantiation code for a Scanner instance.
-        Scanner scanner = new Scanner(System.in);
 
-        // Concatenate the user response to this String.
-        String domo = "Domo arigato ";
+        Scanner userString = new Scanner(System.in);                            //UserString prompt in
 
-        System.out.print("My name is Mr. Roboto, are you Kilroy? ");
-        // Write your program here
+        System.out.println("What is your name?");
+        String userName = userString.nextLine();
+
+        String domo = "Domo arigato ";                                          //String scripts
+        String nameMrRoboto = "My name is Mr. Roboto, are you Kilroy? ";
+        String stan = "no, I'm %s";
+
+        System.out.println(nameMrRoboto + String.format(stan, userName));
+        System.out.println(domo + String.format(stan, userName));
+
+        /*
+        My name is Mr. Roboto, are you Kilroy? no, I'm stan
+        Domo arigato no, I'm stan
+         */
+
     }
 }
 
 /*
-Modify MrRoboto.java to do the following:
-
-Where the comment // Write your program here exists, use the scanner instance to read standard in and wait for the user to press Enter.
 
 After the enter key is pressed, your program should print the string typed by the user, initially you, concatenated to the String variable domo.
 
 See Oracle's Scanner Javadoc to read all the data types Scanner can read (You want to read a String)
 
 Hint: you're looking for a method -> nextLine
-
-Output should look something like this:
-
-My name is Mr. Roboto, are you Kilroy? no, I'm stan
-Domo arigato no, I'm stan
  */
