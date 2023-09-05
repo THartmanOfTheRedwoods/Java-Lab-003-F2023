c7f720bd2ce9fcda147f2e213d756294fc3e44af
Atticus Blanco <atticusbrujx@gmail.com>
2023-09-05 11:49:20 -0700

    Code instructions commented out into code
    Warning:(12, 13) Variable 'number' is never used

--- src/GettingHotInHere.java
+++ src/GettingHotInHere.java
@@ -12,3 +12,29 @@ public class GettingHotInHere {
         int number = scanner.nextInt();
     }
 }
+
+/*
+In this lab implement a program that asks the user for the temperature in °F and converts it to °C.
+
+The function for conversion is: Y°C = (X°F - 32) x (5/9)
+
+Pay attention to integer division!
+
+After you've converted the temperature, print out the following:
+
+The X°F the user typed
+
+The Y°C you converted as a floating point number
+
+The Y°C as an integer
+
+The String "If it were 2°C warmer it would be: 2 + Y°C" (Do the calculation and concatenation)
+
+Output should like something like below for 50°F
+
+What is the temperature in °F: 50
+50°F
+10.000000°C
+10°C
+If it were 2°C warmer it would be: 12.000000°C
+ */
