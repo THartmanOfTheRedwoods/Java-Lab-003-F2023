885535b0bc7f7a51fca4f2459d1338eec347b470
Atticus Blanco <atticusbrujx@gmail.com>
2023-09-05 12:54:40 -0700

    Pseudocode
    Author tags added
    Instructions commented out
    
    Warning:(16, 13) Variable 'number' is never used

--- src/GettingHotInHere.java
+++ src/GettingHotInHere.java
@@ -4,28 +4,32 @@ public class GettingHotInHere {
     /**
      * Teaching Mr. Roboto how to listen to us.
      * @param args Command line arguments [The source file path, The target file path, ...]
+     * @author Trevor Hartman
+     * @author Atticus Blanco
+     *
+     * @since Version 1.0
      **/
     public static void main(String[] args) {
         // The instantiation code for a Scanner instance.
         Scanner scanner = new Scanner(System.in);
-        System.out.print("What is the temperature in °F: ");
-        int number = scanner.nextInt();
+        System.out.print("What is the temperature in °F: ");            //User prompt
+        int number = scanner.nextInt();                                 //User input. change var name to fahrenheitDegree
+
+        //declare variable for celciusDegree
+
+        //do float math and assign to celciusDegree
+
+        //
     }
 }
 
 /*
-In this lab implement a program that asks the user for the temperature in °F and converts it to °C.
-
 The function for conversion is: Y°C = (X°F - 32) x (5/9)
-
-Pay attention to integer division!
 
 After you've converted the temperature, print out the following:
 
 The X°F the user typed
-
 The Y°C you converted as a floating point number
-
 The Y°C as an integer
 
 The String "If it were 2°C warmer it would be: 2 + Y°C" (Do the calculation and concatenation)
