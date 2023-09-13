358efdd296b74531025367a9aba49e8c6bb87240
Kevin <kevinsicairos930@gmail.com>
2023-09-13 13:16:27 -0700

    lab 3 done

--- src/GettingHotInHere.java
+++ src/GettingHotInHere.java
@@ -10,5 +10,12 @@ public class GettingHotInHere {
         Scanner scanner = new Scanner(System.in);
         System.out.print("What is the temperature in °F: ");
         int number = scanner.nextInt();
+        System.out.println(number + "°F");
+        float Y = (number - 32) * 5 / 9;
+        int X =  (number - 32) * 5 / 9;
+        int Z = (number - 32) * 5 / 9 + 2;
+        System.out.printf(Y + "°C \n");
+        System.out.printf(X + "°C \n");
+        System.out.printf("If it were 2°C warmer it would be " + Z + "°C \n");
     }
 }
