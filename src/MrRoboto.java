45b89bd5ad5ff855663b92473b02e3fd6ae655bd
rteefy <rteefy046@mycr.redwoods.edu>
2023-09-14 10:31:13 -0700

    Finished Lab

--- src/MrRoboto.java
+++ src/MrRoboto.java
@@ -1,6 +1,9 @@
 // Import the Scanner tool used for reading (All external packages/libraries/apis are imported similarly this)
 import java.util.Scanner;
 
+import static java.lang.System.in;
+import static java.lang.System.setOut;
+
 public class MrRoboto {
     /**
      * Teaching Mr. Roboto how to listen to us.
@@ -8,12 +11,20 @@ public class MrRoboto {
      **/
     public static void main(String[] args) {
         // The instantiation code for a Scanner instance.
-        Scanner scanner = new Scanner(System.in);
+
 
         // Concatenate the user response to this String.
         String domo = "Domo arigato ";
 
-        System.out.print("My name is Mr. Roboto, are you Kilroy? ");
+
         // Write your program here
+        Scanner scanner = new Scanner(in);
+        System.out.print("My name is Mr. Roboto, are you Kilroy? ");
+        String line = "No, I'm Stan";
+
+        String input = scanner.nextLine();
+        System.out.println(domo + line);
+
     }
+
 }
