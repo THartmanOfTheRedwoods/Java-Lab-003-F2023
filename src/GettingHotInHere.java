dafa17feb4f79b80898f2af10ee340f027a93d1e
Lina <lina.guts12@gmail.com>
2023-09-20 22:36:31 -0700

    Lab003 completed

--- src/GettingHotInHere.java
+++ src/GettingHotInHere.java
@@ -1,3 +1,11 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Angelina Perez
+ *
+ * @since Version 1.0
+ *
+ */
 import java.util.Scanner;
 
 public class GettingHotInHere {
@@ -10,5 +18,17 @@ public class GettingHotInHere {
         Scanner scanner = new Scanner(System.in);
         System.out.print("What is the temperature in °F: ");
         int number = scanner.nextInt();
+
+        // Performing the temperature conversion
+        double celsius = (number - 32) * 5.0 / 9.0;
+
+        // the results from the conversion
+        System.out.println(number + "°F");
+        System.out.printf("%.6f°C%n", celsius);
+        System.out.println((int) celsius + "°C");
+
+        //results if the temperature were 2°C warmer
+        double warmerCelsius = celsius + 2;
+        System.out.println("If it were 2°C warmer it would be: " + String.format("%.6f°C", warmerCelsius));
     }
 }
