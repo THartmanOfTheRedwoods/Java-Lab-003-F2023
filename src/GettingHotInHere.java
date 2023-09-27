3bba7c0cd08ab80f1df915edce4071fa2ae34911
dyoungxboxpro <dyoung367@mycr.redwoods.edu>
2023-09-27 14:20:18 -0700

    completed Lab 3

--- src/GettingHotInHere.java
+++ src/GettingHotInHere.java
@@ -8,7 +8,27 @@ public class GettingHotInHere {
     public static void main(String[] args) {
         // The instantiation code for a Scanner instance.
         Scanner scanner = new Scanner(System.in);
-        System.out.print("What is the temperature in °F: ");
+        System.out.print("What is the temperature in °F:  ");
         int number = scanner.nextInt();
+        System.out.println(number + "°F" );
+
+        double celsius = (number - 32) * (5.0/9);
+        System.out.printf("%.3f°C\n", celsius );
+        int ci = (int) celsius;
+        System.out.println(ci + "°C");
+        double celsius2 = celsius + 2;
+        String A = "If it were 2°C warmer it would be";
+        System.out.printf("%s %.3f°C\n", A, celsius2);
+
+
+
+
+
+
+
+
+
+
+
     }
 }
