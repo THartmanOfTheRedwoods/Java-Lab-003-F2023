9a4800fcb53f74f3a93d040e7e6f491db934a714
Joseph Verdin <josephverdin55@gmail.com>
2023-09-30 22:04:27 -0700

    Joseph Verdin not sure if i did this how you wanted

--- src/GettingHotInHere.java
+++ src/GettingHotInHere.java
@@ -9,6 +9,13 @@ public class GettingHotInHere {
         // The instantiation code for a Scanner instance.
         Scanner scanner = new Scanner(System.in);
         System.out.print("What is the temperature in °F: ");
-        int number = scanner.nextInt();
+        int degf = scanner.nextInt();
+        System.out.println(degf + "°F");
+        int ftoc = ((degf - 32) * 5/9);
+        float ftocf = ftoc;
+        System.out.println(ftocf + "°C");
+        System.out.println(ftoc + "°C");
+        float twowarmer = ftocf + 2;
+        System.out.println("If it were 2°C warmer it would be: " + twowarmer + "°C");
     }
 }
